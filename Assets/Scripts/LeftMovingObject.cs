e340fbffc82546ad028676114c9382ac070381d0
AleksandrosP38 <alexandrepacheco2000@hotmail.com>
2023-10-27 15:54:52 +0100

    update

--- Assets/Scripts/LeftMovingObject.cs
+++ Assets/Scripts/LeftMovingObject.cs
@@ -5,11 +5,8 @@ public class LeftMovingObject : MonoBehaviour
     public float moveSpeed = 5f; // Speed at which the object moves to the left
     public float minY = 1.0f;    // Minimum Y position
     public float maxY = 4.0f;    // Maximum Y position
-    public float destroyTime = 10f; // Time after which the object will be destroyed (in seconds)
 
     private Vector2 spawnPosition;
-    private float elapsedTime = 0f;
-
     public bool moveLeft;
 
     void Start()
@@ -34,14 +31,5 @@ public class LeftMovingObject : MonoBehaviour
         }
        
 
-        // Increment the elapsed time
-        elapsedTime += Time.deltaTime;
-
-        // Check if the elapsed time has exceeded the destroyTime
-        if (elapsedTime >= destroyTime)
-        {
-            // Destroy the game object
-            Destroy(gameObject);
-        }
     }
 }
