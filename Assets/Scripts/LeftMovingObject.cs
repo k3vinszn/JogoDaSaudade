71c8ccd60d11e03fb9a9377a4f1753e0c81814cd
AleksandrosP38 <alexandrepacheco2000@hotmail.com>
2023-10-27 12:15:42 +0100

    update

--- Assets/Scripts/LeftMovingObject.cs
+++ Assets/Scripts/LeftMovingObject.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class LeftMovingObject : MonoBehaviour
 {
     public float moveSpeed = 5f; // Speed at which the object moves to the left
-    public float minY = 1.0f;   // Minimum Y position
-    public float maxY = 4.0f;   // Maximum Y position
+    public float minY = 1.0f;    // Minimum Y position
+    public float maxY = 4.0f;    // Maximum Y position
+    public float destroyTime = 10f; // Time after which the object will be destroyed (in seconds)
 
     private Vector2 spawnPosition;
+    private float elapsedTime = 0f;
 
     void Start()
     {
@@ -20,5 +22,15 @@ public class LeftMovingObject : MonoBehaviour
     {
         // Move the object to the left
         transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+        // Increment the elapsed time
+        elapsedTime += Time.deltaTime;
+
+        // Check if the elapsed time has exceeded the destroyTime
+        if (elapsedTime >= destroyTime)
+        {
+            // Destroy the game object
+            Destroy(gameObject);
+        }
     }
 }
